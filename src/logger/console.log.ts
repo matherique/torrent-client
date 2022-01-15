04d17c666929fd1875219286deae5a12ffb39f00
Matheus Henrique <matherique@gmail.com>
2022-01-15 17:31:30 -0300

    remove all files from ts-version

--- src/logger/console.log.ts
+++
@@ -1,6 +0,0 @@
-export default function log(text: string, data: any[], callback?: () => void): void {
-  console.log('\x1b[31m%s\x1b[0m', text, ...data);  // Red
-
-  if (callback) 
-    callback();
-}
